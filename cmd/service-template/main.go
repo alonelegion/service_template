2fcc6f8fe5e246279c84dc731e559824f07b18d7
AloneLegion <com.berdin@gmail.com>
2020-07-29 19:05:22 +0300

    add env file

--- cmd/service-template/main.go
+++ cmd/service-template/main.go
@@ -3,8 +3,16 @@ package service_template
 import (
 	"context"
 	"github.com/alonelegion/service_template/internal/shutdown_service"
+	"log"
 	"math/rand"
+	"os"
 	"time"
+
+	ll "github.com/alonelegion/service_template/internal/app/logger"
+)
+
+const (
+	Name = "service-template"
 )
 
 func main() {
@@ -14,5 +22,15 @@ func main() {
 	ctx := shutdown_service.ShutDownContext(context.Background())
 
 	// logger
-	logger, err :=
+	logger, err := ll.New(
+		Name,
+		os.Getenv("VERSION"),
+		os.Getenv("ENV"),
+		os.Getenv("LOG_LEVEL"),
+	)
+
+	if err != nil {
+		// Ошибка при инициализации логгера
+		log.Fatal("error while init logger")
+	}
 }
